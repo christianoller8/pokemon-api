875eafd2339d1a6ffecf21d407d47d8670202f3d
Sandro34BM <sandrobortolotti34@gmail.com>
2023-05-02 10:43:01 +0200

    Added news

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { LandingRoutingModule } from "./landing-routing.module";
 import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
-  declarations: [LandingPageComponent, CarouselComponent],
+  declarations: [LandingPageComponent, CarouselComponent, NewsComponent],
   imports: [CommonModule, LandingRoutingModule],
   exports: [LandingPageComponent],
 })
